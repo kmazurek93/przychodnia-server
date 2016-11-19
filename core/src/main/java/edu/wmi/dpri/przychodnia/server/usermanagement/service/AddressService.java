5f84ebbc86e3628b97e696e902b69169e112cf3e
Karol Mazurek <karmaz@st.amu.edu.pl>
2016-11-19 17:39:21 +0100

    LazyInitializationException Fix

--- core/src/main/java/edu/wmi/dpri/przychodnia/server/usermanagement/service/AddressService.java
+++ core/src/main/java/edu/wmi/dpri/przychodnia/server/usermanagement/service/AddressService.java
@@ -2,8 +2,10 @@ package edu.wmi.dpri.przychodnia.server.usermanagement.service;
 
 import edu.wmi.dpri.przychodnia.commons.usermanagement.webmodel.AddressWebModel;
 import edu.wmi.dpri.przychodnia.server.entity.Address;
+import edu.wmi.dpri.przychodnia.server.entity.Person;
 import edu.wmi.dpri.przychodnia.server.exceptionmanagement.ExceptionCause;
 import edu.wmi.dpri.przychodnia.server.repository.AddressRepository;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -85,6 +87,12 @@ public class AddressService {
     public List<Address> searchQueryOnAll(String likeString) {
         List<Address> addresses = repository.searchQueryOnAll(likeString);
         addresses.forEach(adr -> initialize(adr.getPersonsWithMailingAddress()));
+        addresses.forEach(adr -> {
+            List<Person> persons = adr.getPersons();
+            persons.forEach(person -> Hibernate.initialize(person.getUsers()));
+            List<Person> personsWithMailingAddress = adr.getPersonsWithMailingAddress();
+            personsWithMailingAddress.forEach(person -> Hibernate.initialize(person.getUsers()));
+        });
         return addresses;
     }
 }
