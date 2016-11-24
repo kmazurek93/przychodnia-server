8338c6be50457607607c3bde58c6d37f687bcaa9
Karol Mazurek <karmaz@st.amu.edu.pl>
2016-11-24 19:33:10 +0100

    RoleAssignment Api (part I)

--- commons/src/main/java/edu/wmi/dpri/przychodnia/commons/usermanagement/webapi/RoleManagementWebApi.java
+++ commons/src/main/java/edu/wmi/dpri/przychodnia/commons/usermanagement/webapi/RoleManagementWebApi.java
@@ -1,7 +1,35 @@
 package edu.wmi.dpri.przychodnia.commons.usermanagement.webapi;
 
+import edu.wmi.dpri.przychodnia.commons.usermanagement.webmodel.RoleAssignmentWebModel;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static edu.wmi.dpri.przychodnia.commons.usermanagement.UserManagementConstants.API_PATH;
+import static edu.wmi.dpri.przychodnia.commons.usermanagement.webapi.RoleManagementWebApi.BASE_PATH;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 /**
  * Created by kmazu on 02.07.2016.
  */
+@Path(API_PATH + BASE_PATH)
 public interface RoleManagementWebApi {
+    String BASE_PATH = "/roles";
+
+    @GET
+
+    @Produces(APPLICATION_JSON)
+    List<String> getAllPossibleRoles();
+
+    @POST
+    @Path("/assign")
+    @Consumes(APPLICATION_JSON)
+    Response assignRole(RoleAssignmentWebModel roleAssignmentWebModel);
+
+    @POST
+    @Path("/unassign")
+    @Consumes(APPLICATION_JSON)
+    Response unassignRole(RoleAssignmentWebModel roleAssignmentWebModel);
+
 }
---
+++ core/src/main/java/edu/wmi/dpri/przychodnia/server/usermanagement/web/service/RoleWebService.java
@@ -0,0 +1,63 @@
+package edu.wmi.dpri.przychodnia.server.usermanagement.web.service;
+
+import edu.wmi.dpri.przychodnia.commons.usermanagement.webmodel.RoleAssignmentWebModel;
+import edu.wmi.dpri.przychodnia.server.entity.Role;
+import edu.wmi.dpri.przychodnia.server.exceptionmanagement.exceptions.auth.ForbiddenException;
+import edu.wmi.dpri.przychodnia.server.usermanagement.service.RoleAssigningService;
+import edu.wmi.dpri.przychodnia.server.usermanagement.service.RoleService;
+import edu.wmi.dpri.przychodnia.server.usermanagement.service.verification.UserVerificationService;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static edu.wmi.dpri.przychodnia.server.exceptionmanagement.generators.ErrorMessageGenerator.getForbiddenErrorMessage;
+import static edu.wmi.dpri.przychodnia.server.security.model.RoleAuthority.STAFF_OR_ADMIN;
+
+/**
+ * Created by lupus on 24.11.16.
+ */
+@Component
+public class RoleWebService {
+
+    @Inject
+    private RoleService roleService;
+    @Inject
+    private UserVerificationService userVerificationService;
+    @Inject
+    private RoleAssigningService roleAssigningService;
+
+    public List<String> getAllPossibleRoles() {
+        boolean isAdminOrStaff = userVerificationService.verifyIfHasAnyAuthorityOf(STAFF_OR_ADMIN);
+        if (isAdminOrStaff) {
+            return roleService.findAllUnInitialized()
+                    .stream().map(Role::getName)
+                    .collect(Collectors.toList());
+        } else {
+            throw new ForbiddenException(getForbiddenErrorMessage("INSUFFICIENT_PRIVILEGES"));
+        }
+    }
+
+    public Response assignRole(RoleAssignmentWebModel roleAssignmentWebModel) {
+        boolean isAdminOrStaff = userVerificationService.verifyIfHasAnyAuthorityOf(STAFF_OR_ADMIN);
+        if (isAdminOrStaff) {
+            roleAssigningService.assignRole(roleAssignmentWebModel);
+            return Response.noContent().build();
+        } else {
+            throw new ForbiddenException(getForbiddenErrorMessage("INSUFFICIENT_PRIVILEGES"));
+        }
+    }
+
+    public Response unassignRole(RoleAssignmentWebModel roleAssignmentWebModel) {
+        boolean isAdminOrStaff = userVerificationService.verifyIfHasAnyAuthorityOf(STAFF_OR_ADMIN);
+        if (isAdminOrStaff) {
+            roleAssigningService.unassignRole(roleAssignmentWebModel);
+            return Response.noContent().build();
+        } else {
+            throw new ForbiddenException(getForbiddenErrorMessage("INSUFFICIENT_PRIVILEGES"));
+        }
+    }
+
+}
