941ecdb5f8e39ffd14acb05121fb74430448cea9
Karol Mazurek <karmaz@st.amu.edu.pl>
2016-12-31 15:38:57 +0100

    Added missing Annotations @Consumes @Produces

--- commons/src/main/java/edu/wmi/dpri/przychodnia/commons/visits/webapi/VisitsWebApi.java
+++ commons/src/main/java/edu/wmi/dpri/przychodnia/commons/visits/webapi/VisitsWebApi.java
@@ -1,29 +1,47 @@
 package edu.wmi.dpri.przychodnia.commons.visits.webapi;
 
-import edu.wmi.dpri.przychodnia.commons.visits.webmodel.*;
+import edu.wmi.dpri.przychodnia.commons.visits.webmodel.AvailableTimeRequestModel;
+import edu.wmi.dpri.przychodnia.commons.visits.webmodel.DayWebModel;
+import edu.wmi.dpri.przychodnia.commons.visits.webmodel.MonthWebModel;
+import edu.wmi.dpri.przychodnia.commons.visits.webmodel.SimpleAvailabilityWebModel;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import java.util.List;
 import java.util.Map;
+
+import static edu.wmi.dpri.przychodnia.commons.visits.webapi.VisitsWebApi.BASE_PATH;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 /**
  * Created by lupus on 25.11.16.
  */
+@Path(BASE_PATH)
 public interface VisitsWebApi {
 
     String BASE_PATH = "/visits";
+    String MONTHS = "/months";
+    String DAYS = "/days";
+    String CALENDAR = "/calendar";
 
     @POST
-    @Path("/months")
+    @Path(MONTHS)
+    @Produces(APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
     List<MonthWebModel> getNextAvailableMonthsForDoctor(AvailableTimeRequestModel model);
 
     @POST
-    @Path("/days")
+    @Path(DAYS)
+    @Produces(APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
     List<DayWebModel> getAvailableDaysForDoctorsMonth(AvailableTimeRequestModel model);
 
     @POST
-    @Path("/calendar")
+    @Path(CALENDAR)
+    @Produces(APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
     Map<String, List<SimpleAvailabilityWebModel>> getCalendarForDoctor(AvailableTimeRequestModel model);
 
 
