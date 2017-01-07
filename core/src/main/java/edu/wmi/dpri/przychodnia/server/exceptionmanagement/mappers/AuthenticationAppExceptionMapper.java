4e3ab47a86d06321a30162da0e246251e5498cfc
Karol Mazurek <kmazurek93@gmail.com>
2017-01-07 12:05:28 +0100

    One Logger removal

--- core/src/main/java/edu/wmi/dpri/przychodnia/server/exceptionmanagement/mappers/AuthenticationAppExceptionMapper.java
+++ core/src/main/java/edu/wmi/dpri/przychodnia/server/exceptionmanagement/mappers/AuthenticationAppExceptionMapper.java
@@ -1,8 +1,6 @@
 package edu.wmi.dpri.przychodnia.server.exceptionmanagement.mappers;
 
 import edu.wmi.dpri.przychodnia.server.exceptionmanagement.exceptions.auth.AuthenticationAppException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,11 +10,9 @@ import javax.ws.rs.ext.ExceptionMapper;
  * Created by lupus on 22.10.16.
  */
 public class AuthenticationAppExceptionMapper implements ExceptionMapper<AuthenticationAppException> {
-    private static final Logger logger = LoggerFactory.getLogger(AuthenticationAppException.class);
 
     @Override
     public Response toResponse(AuthenticationAppException ex) {
-        logger.error("Authentication exception: ", ex);
         return Response.status(ex.getErrorMessage().getStatus())
                 .entity(ex.getErrorMessage())
                 .type(MediaType.APPLICATION_JSON)
