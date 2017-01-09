43b580c7ceb0444b6b0a0e7df95e61889e867cdb
Karol Mazurek <karmaz@st.amu.edu.pl>
2017-01-09 07:17:36 +0100

    character encoding filter

--- core/src/main/java/edu/wmi/dpri/przychodnia/server/PrzychodniaServerApplicationRunner.java
+++ core/src/main/java/edu/wmi/dpri/przychodnia/server/PrzychodniaServerApplicationRunner.java
@@ -3,11 +3,15 @@ package edu.wmi.dpri.przychodnia.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * Created by lupus on 21.09.16.
@@ -28,5 +32,14 @@ public class PrzychodniaServerApplicationRunner extends SpringBootServletInitial
         application.profiles(System.getProperty("spring.profiles.active").split(","));
         System.out.println("Profiles loaded: " + System.getProperty("spring.profiles.active"));
         return application.sources(PrzychodniaServerApplicationRunner.class);
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
     }
 }
